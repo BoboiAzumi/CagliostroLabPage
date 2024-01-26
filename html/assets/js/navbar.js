096b0e7434bb1c1e8cd8a00048ab5a428660c398
BoboiAzumi <untukherokudeploy@gmail.com>
2024-01-26 10:42:41 +0700

    New Project

---
+++ html/assets/js/navbar.js
@@ -0,0 +1,23 @@
+let btn_1 = document.getElementById("btn_1")
+        let mobile_navbar = document.getElementById("mobile_navbar")
+        let btn_1_toggle = false
+        let btn_1_class = btn_1.getAttribute("class")
+        function toggle(){
+            if(btn_1_toggle){
+                btn_1.setAttribute("class", btn_1_class)
+                //mobile_navbar.style = "display:none"
+                mobile_navbar.classList.toggle("translate-y-16")
+                mobile_navbar.classList.toggle("translate-y-[-100rem]")
+                btn_1_toggle = !btn_1_toggle
+            }
+            else{
+                btn_1.setAttribute("class", btn_1_class+" rotate-90")
+                //mobile_navbar.style = "display:flex"
+                mobile_navbar.classList.toggle("translate-y-16")
+                mobile_navbar.classList.toggle("translate-y-[-100rem]")
+                btn_1_toggle = !btn_1_toggle
+            }
+        }
+        btn_1.addEventListener("click", (e) => {
+            toggle()
+        })
---
+++ html/assets/js/stickynavbar.js
@@ -0,0 +1,13 @@
+let navbar = document.getElementById("navbar")
+        window.onscroll = function() {
+            navbarsticky();
+        }
+
+        function navbarsticky(){
+            if(window.pageYOffset > navbar.offsetTop){
+                navbar.classList.add("fixed")
+            }
+            else{
+                navbar.classList.remove("fixed")
+            }
+        }
